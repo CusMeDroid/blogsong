fd48e1d6b35ffe004715af7d513384311bb3c5c7
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2023-12-13 20:07:30 +0700

    Delete assets directory

--- assets/hamburger/index.js
+++
@@ -1,8 +0,0 @@
-function myFunction(burger) {
-  burger.classList.toggle("change");
-}
-$(document).ready(function(){
-  $("#smenu").click(function(){
-    $("#menu-show").slideToggle('fast');
-  });
-});
